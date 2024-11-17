9c3ba4e5eff8b32e2bfa5d8fcce4de5b74b5c04b
Rabiul Hasan Rafee <rhr277@gmail.com>
2024-11-17 20:44:38 +0600

    example of HOC done

--- src/Components/ClickCounter.jsx
+++ src/Components/ClickCounter.jsx
@@ -1,10 +1,16 @@
+/*eslint-disable */
+
 import withCounter from "./HOC/withCounter";
 
-const ClickCounter = ({ count, incrementCount }) => {
+const ClickCounter = ({ count, incrementCount, hover, incrementHover }) => {
   return (
     <>
-      <button type="button" onClick={incrementCount}>
-        Clicked {count} times
+      <button
+        type="button"
+        onClick={incrementCount}
+        onMouseOver={incrementHover}
+      >
+        Clicked {count} times Hovered {hover} times
       </button>
     </>
   );
--- src/Components/HOC/withCounter.jsx
+++ src/Components/HOC/withCounter.jsx
@@ -3,13 +3,26 @@ import { useState } from "react";
 const withCounter = (OriginalComponent) => {
   const WrappedComponent = (props) => {
     const [count, setCount] = useState(0);
+    const [hover, setHover] = useState(0);
 
-    const increment = () => {
+    // Increment the click count
+    const incrementClick = () => {
       setCount((prevState) => prevState + 1);
     };
 
+    // Increment the hover count
+    const incrementHover = () => {
+      setHover((prevState) => prevState + 1);
+    };
+
     return (
-      <OriginalComponent count={count} incrementCount={increment} {...props} />
+      <OriginalComponent
+        count={count}
+        incrementCount={incrementClick}
+        hover={hover}
+        incrementHover={incrementHover}
+        {...props}
+      />
     );
   };
 
